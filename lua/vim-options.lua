e659873e70331c4463b787cd06630b2eb84832dc
FLIVLA <fli@vla.dk>
2024-03-13 14:14:15 +0100

    nvim-config

---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("let g:loaded_perl_provider = 0")
+vim.cmd("let g:loaded_ruby_provider = 0")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+vim.opt.wrap = false
+
+vim.g.mapleader = " "
